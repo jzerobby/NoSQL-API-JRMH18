dc352f0b3186f46cc927feab0b57185e5f4460fb
JZERO MAKILING <jzerorobby07092016@gmail.com>
2023-08-27 14:42:19 -0700

    updated utils folder and seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,61 +1,29 @@
-const mongoose = require('mongoose');
-const User = require('./models/User');
-const Thought = require('./models/Thought');
+const connection = require('../config/connection');
+const { User, Thought} = require('../models')
 
-// Connect to your MongoDB database
-mongoose.connect('mongodb://localhost:27017/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+connection.on('error', (err) => err);
 
-// Sample data for users and thoughts
-const usersData = [
-  {
-    username: 'user1',
-    email: 'user1@example.com'
-  },
-  {
-    username: 'user2',
-    email: 'user2@example.com'
-  }
-];
+connection.once('open', async () => {
+  console.log('connected');
 
-const thoughtsData = [
-  {
-    thoughtText: 'This is thought 1 by user1',
-    username: 'user1'
-  },
-  {
-    thoughtText: 'This is thought 2 by user2',
-    username: 'user2'
-  }
-];
+  await Thought.deleteMany({});
 
-// Function to seed the database with sample data
-const seedDatabase = async () => {
-  try {
-    // Clear existing data
-    await User.deleteMany();
-    await Thought.deleteMany();
+  await User.deleteMany({});
 
-    // Create new users
-    const users = await User.create(usersData);
+  const users = [
+    {
+      username: "Robby",
+      email: "robby@email.com"
+    },
+    {
+      username: "Jzero",
+      email: "jzero@email.com"
+    }
+  ];
 
-    // Create new thoughts and associate with users
-    const thoughts = await Promise.all(thoughtsData.map(async (thoughtData) => {
-      const user = users.find(user => user.username === thoughtData.username);
-      thoughtData.userId = user._id;
-      return await Thought.create(thoughtData);
-    }));
+  await User.collection.insertMany(users);
 
-    console.log('Database seeded successfully');
-  } catch (err) {
-    console.error('Error seeding database:', err);
-  } finally {
-    // Close the database connection
-    mongoose.disconnect();
-  }
-};
-
-// Call the seedDatabase function to seed the data
-seedDatabase();
+  console.table(users);
+  console.info('Seeding completed!');
+  process.exit(0);
+})
